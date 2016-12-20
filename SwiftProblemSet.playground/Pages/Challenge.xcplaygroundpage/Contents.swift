13fde3a35161baa0e8cb02ce430229fd2b9e57e2
manujua82 <manujua@gmail.com>
2016-12-20 17:47:27 -0500

    Challenge completed

--- SwiftProblemSet.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ SwiftProblemSet.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -9,10 +9,11 @@ import Foundation
 //: Mystery code! What does this code do? Briefly, using comments, describe what is happening in each line. **Hint**: You may need to look up [Int initializers](http://stackoverflow.com/questions/30739460/toint-removed-in-swift-2).
 let array = ["A", "13", "B", "5", "87", "t", "41"]  //Initialize a string type constant array.
 var sum = 0 // Initialize a variable interger type.
-for string in array { //loop iterate over the variable array.
-    if Int(string) != nil { //validate if string value is of type Integer.
-        let intToAdd = Int(string)! //converts string value to Integer.
-        sum += intToAdd //sum the intToAdd variable with the sum variable.
+for string in array { //loop to iterate over the variable array.
+    if Int(string) != nil { //validate if string value is integer type.
+        let intToAdd = Int(string)! //converts string value to integer.
+        sum += intToAdd //sum var is the sum of sum var plus intToAdd var.
+
     }
 }
 print(sum) //print sum variable.
